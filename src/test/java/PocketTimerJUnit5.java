79618fec582d0193e397a462f94df9e3ac20c42d
Richard Wyant <rwyant@riseup.net>
2017-06-07 09:25:12 -0500

    Initial commit

---
+++ src/test/java/PocketTimerJUnit5.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Richard on 6/7/2017.
+ */
+class PocketTimerJUnit5 {
+    @RunWith(JUnitPlatform.class)
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("JUnit 5 test");
+    }
+
+}
