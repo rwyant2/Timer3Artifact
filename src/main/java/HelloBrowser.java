ac2e2bd1db38b389a4211285533a12a17312c1a3
Richard Wyant <rwyant@riseup.net>
2017-06-14 17:10:06 -0500

    Adjusted libraries, started spring boot work.

--- src/main/java/HelloBrowser.java
+++ src/main/java/HelloBrowser.java
@@ -1,8 +1,13 @@
+/*import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * Created by Richard on 6/10/2017.
  */
 public class HelloBrowser {
-    public static void main () {
+    public static void main (String args[]) {
         System.out.println("This should be in a browser");
     }
 }
